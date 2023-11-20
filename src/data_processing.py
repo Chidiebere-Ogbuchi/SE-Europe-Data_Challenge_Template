0e9c36406adadac342bef07e0fb3c8ee12f46bb3
mirwisek <mirwise001@gmail.com>
2023-11-21 00:37:14 +0100

    Implementation of imputation extended for LOAD data

--- src/data_processing.py
+++ src/data_processing.py
@@ -16,21 +16,47 @@ def clean_data(df):
         df['EndTime'] = pd.to_datetime(df['EndTime'], format='%Y-%m-%dT%H:%M+00:00Z')
     return df
 
-def add_missing_interval_rows(df, minutes=15):
+def add_missing_interval_rows(df, dtype, minutes):
 
     start_date = pd.to_datetime('2021-12-31 00:45:00')
-    end_date = pd.to_datetime('2023-01-01 00:00:00')
 
     df.drop(columns=['FloorEndTime'], inplace=True)
 
-    # Iterate over each unique 'PsrType'
-    for psrtype in df['PsrType'].unique():
-        # Filter rows for the specific 'PsrType' and within the date range
-        psrtype_df = df[(df['PsrType'] == psrtype)]
+    if dtype == 'GEN':
+        # Iterate over each unique 'PsrType'
+        for psrtype in df['PsrType'].unique():
+            # Filter rows for the specific 'PsrType' and within the date range
+            psrtype_df = df[(df['PsrType'] == psrtype)]
 
+            # Check if 'StartTime' is incremental by 15 minutes
+            expected_start_time = start_date
+            for index, row in psrtype_df.iterrows():
+                if row['StartTime'] != expected_start_time:
+                    # Create new row with incremental time
+                    new_row = {
+                        'StartTime': expected_start_time,
+                        'EndTime': expected_start_time + pd.Timedelta(minutes),
+                        'AreaID': row['AreaID'],
+                        'UnitName': row['UnitName'],
+                        'PsrType': row['PsrType'],
+                        'quantity': np.nan,  # Set quantity as NaN
+                        'CountryID':row['CountryID']
+                    }
+                    # Append the new row to the DataFrame
+                    df = pd.concat([df, pd.DataFrame([new_row])], ignore_index=True)
+
+                    # Update expected_start_time for the next iteration
+                    expected_start_time += pd.Timedelta(minutes)
+
+                # Break and start with the next row
+                expected_start_time = row['EndTime']
+
+        # Sort the DataFrame based on 'PsrType' and 'StartTime'
+        df.sort_values(by=['PsrType', 'StartTime'], inplace=True, ignore_index=True)
+    else: # LOAD 
         # Check if 'StartTime' is incremental by 15 minutes
         expected_start_time = start_date
-        for index, row in psrtype_df.iterrows():
+        for _, row in df.iterrows():
             if row['StartTime'] != expected_start_time:
                 # Create new row with incremental time
                 new_row = {
@@ -38,11 +64,11 @@ def add_missing_interval_rows(df, minutes=15):
                     'EndTime': expected_start_time + pd.Timedelta(minutes),
                     'AreaID': row['AreaID'],
                     'UnitName': row['UnitName'],
-                    'PsrType': row['PsrType'],
-                    'quantity': np.nan,  # Set quantity as NaN
-                    'CountryID':row['CountryID']
+                    'Load': np.nan,  # Set quantity as NaN
+                    'CountryID': row['CountryID']
                 }
                 # Append the new row to the DataFrame
+                print(f"added rows: {new_row['StartTime']}")
                 df = pd.concat([df, pd.DataFrame([new_row])], ignore_index=True)
 
                 # Update expected_start_time for the next iteration
@@ -51,51 +77,51 @@ def add_missing_interval_rows(df, minutes=15):
             # Break and start with the next row
             expected_start_time = row['EndTime']
 
-    # Sort the DataFrame based on 'PsrType' and 'StartTime'
-    df.sort_values(by=['PsrType', 'StartTime'], inplace=True, ignore_index=True)
+        # Sort the DataFrame based on 'StartTime'
+        df.sort_values(by=['StartTime'], inplace=True, ignore_index=True)
 
     df['FloorEndTime'] = df['EndTime'].dt.floor('H')
 
     return df
 
-def impute_values(df):
+def impute_values(df, variable_to_impute):
         
     start_date = pd.to_datetime('2022-01-01 00:00:00')
     end_date = pd.to_datetime('2023-01-01 00:00:00')
 
     df = df[(df['FloorEndTime'] >= start_date) & (df['FloorEndTime'] <= end_date)]
     try:
-        df['quantity'] = df['quantity'].astype(float)
+        df[variable_to_impute] = df[variable_to_impute].astype(float)
     except ValueError:
         # For typical nans filled with strings replace with nans
         for char in ['-', '--', '?']:
-            df['quantity'] = df['quantity'].replace(char, np.nan)
+            df[variable_to_impute] = df[variable_to_impute].replace(char, np.nan)
 
         # Set object type to float
-        df['quantity'] = df['quantity'].astype(float)
+        df[variable_to_impute] = df[variable_to_impute].astype(float)
 
-    # Interpolate missing values in 'quantity'
-    df['quantity'].interpolate(method='linear', axis=0, limit_direction='both', inplace=True)
+    # Interpolate missing values in variable_to_impute
+    df[variable_to_impute].interpolate(method='linear', axis=0, limit_direction='both', inplace=True)
     return df
 
-def drop_additional_columns(df):
+def drop_additional_columns(df, datatype):
     # Drop specified columns
     df.drop(columns=['StartTime', 'EndTime', 'AreaID', 'UnitName'], inplace=True)
 
-    # Filter rows based on the specified Psrtype values
-    return df[df['PsrType'].isin(psrtype_values)]
+    if datatype == 'GEN':
+        # Filter rows based on the specified Psrtype values
+        return df[df['PsrType'].isin(psrtype_values)]
+    return df # For load
 
-def aggregate_quantity_by_hour(df):
-    df = df.drop_duplicates(subset=['PsrType', 'CountryID', 'FloorEndTime'])
-    return df.groupby(['PsrType', 'CountryID', 'FloorEndTime'])['quantity'].sum().reset_index()
+def aggregate_quantity_by_hour(df, datatype, variable):
+    if datatype == 'GEN':
+        df = df.drop_duplicates(subset=['PsrType', 'CountryID', 'FloorEndTime'])
+        return df.groupby(['PsrType', 'CountryID', 'FloorEndTime'])[variable].sum().reset_index()
+    df = df.drop_duplicates(subset=['CountryID', 'FloorEndTime'])
+    return df.groupby(['CountryID', 'FloorEndTime'])[variable].sum().reset_index()
 
 
-def preprocess_data(df, minutes, data_type=0):
-    # Check if 'load' column exists and create it if it doesn't for 'load' data type
-    # if data_type == 'load' and 'load' not in df.columns:
-    #     df['load'] = 0
-
-    # df['Interval'] = (df['EndTime'] - df['StartTime']).dt.total_seconds() / 3600
+def preprocess_data(df, datatype, minutes):
 
     # Filter data for the specified date range
     start_date = pd.to_datetime('2021-12-31 00:45:00')
@@ -105,9 +131,11 @@ def preprocess_data(df, minutes, data_type=0):
     # Temporarily floor it for finding np slots, we drop it instantly
     df['FloorEndTime'] = df['EndTime'].dt.floor('H')
 
-    df = add_missing_interval_rows(df, minutes)
-    df = impute_values(df)
-    df = drop_additional_columns(df)
+    variable = 'quantity' if datatype == 'GEN' else 'Load'
+
+    df = add_missing_interval_rows(df, datatype, minutes)
+    df = impute_values(df, variable)
+    df = drop_additional_columns(df, datatype)
 
     return df
 
@@ -130,7 +158,8 @@ def parse_arguments():
     )
     return parser.parse_args()
 
-def main(input_folder='./data/gen_data/', output_dir='./processed_data/'):
+# Spain in gen data is 15min based
+def process_gen_data(input_folder, output_dir):
     # Initialize an empty DataFrame to store aggregated results
     aggregated_data = pd.DataFrame()
 
@@ -140,7 +169,8 @@ def main(input_folder='./data/gen_data/', output_dir='./processed_data/'):
             file_path = os.path.join(input_folder, filename)
             # Extract type and country information from the file name
             file_parts = filename.split('.')[0].split('_')
-            data_type = file_parts[0]  # Extracting 'gen' or 'load'
+            
+            datatype = file_parts[0].upper()  # Extracting 'gen' or 'load'
             country = file_parts[1]    # Extracting country code
             psrtype = file_parts[2]    # Extracting country code
             if not psrtype in psrtype_values:
@@ -152,31 +182,21 @@ def main(input_folder='./data/gen_data/', output_dir='./processed_data/'):
             # Read the CSV file
             df = load_data(file_path)
 
-            # if 'PsrType' not in df.columns:
-            #     df["PsrType"] = 'LOAD'
-            # if 'quantity' not in df.columns:
-            #     df["quantity"] = 0
-            # if 'Load' not in df.columns:
-            #     df["Load"] = 0
-
-
-            # Add 'type' and 'country' columns based on file name information
-            # df['DataType'] = data_type
             df['CountryID'] = country
             
             # Clean and preprocess the data
             df = clean_data(df)
             # NE in filenames
             if country in ['DE', 'SP', 'NE', 'HU']: # 15min
-                df = preprocess_data(df, minutes=15)
+                df = preprocess_data(df, datatype, minutes=15)
             elif country in ['UK']: # 30min
-                df = preprocess_data(df, minutes=30)
+                df = preprocess_data(df, datatype, minutes=30)
             elif country in ['IT', 'DK', 'PO', 'SE']: # 1h
-                df = preprocess_data(df, minutes=60)
+                df = preprocess_data(df, datatype, minutes=60)
             else:
                 raise Exception(f'{country} Country not found in the list')
             
-            df = aggregate_quantity_by_hour(df)
+            df = aggregate_quantity_by_hour(df, datatype, 'quantity')
             # Append the current DataFrame to the aggregated_data
             aggregated_data = pd.concat([aggregated_data, df])
 
@@ -184,29 +204,54 @@ def main(input_folder='./data/gen_data/', output_dir='./processed_data/'):
     if(aggregated_data.shape[0] > 0):
         for country_id, group_data in aggregated_data.groupby('CountryID'):
             # Formulate the output file path based on the country_id
-            output_file = os.path.join(output_dir, f'{country_id}.csv')
+            output_file = os.path.join(output_dir, f'GEN_{country_id}.csv')
             
             # Save the group_data to the file
             save_data(group_data, output_file)
-        print('Process Completed, file saved')
+        print('Gen files processed, file saved')
     else:
         print('Empty DataFrame')
 
-    # Group by required columns and sum the quantity while getting the first 'EndTime'
-    # grouped_data = aggregated_data.groupby(['Country', 'DataType', 'AreaID', 'PsrType', pd.Grouper(key='StartTime', freq='1H')]).agg({
-    #     'quantity': 'sum',
-    #     'Load': 'sum',
-    #     'EndTime': 'first'
-    # }).reset_index()
-    # grouped_data["total"] = grouped_data["quantity"].where(grouped_data["quantity"] > 0, grouped_data["Load"])
-    # # Reorder columns
-    # grouped_data = grouped_data[['Country', 'DataType', 'AreaID', 'StartTime', 'EndTime', 'PsrType', 'total']]
+# Spain in load data is hourly based
+def process_load_data(input_folder, output_dir):
 
-    # # Save the grouped data to an output file
-    # save_data(grouped_data, output_file)
+    # Loop through files in the folder
+    for filename in os.listdir(input_folder):
+        if filename.endswith('.csv'):
+            file_path = os.path.join(input_folder, filename)
+            # Extract type and country information from the file name
+            file_parts = filename.split('.')[0].split('_')
+            country = file_parts[1]    # Extracting country code
+            print(f'Processing {filename}...')
+            
+            # Read the CSV file
+            df = load_data(file_path)
+
+            df['CountryID'] = country
+            
+            # Clean and preprocess the data
+            df = clean_data(df)
+            # NE in filenames
+            if country in ['DE', 'NE', 'HU']: # 15min
+                df = preprocess_data(df, 'LOAD', minutes=15)
+            elif country in ['UK']: # 30min
+                df = preprocess_data(df, 'LOAD', minutes=30)
+            elif country in ['IT', 'DK', 'PO', 'SE', 'SP']: # 1h
+                df = preprocess_data(df, 'LOAD', minutes=60)
+            else:
+                raise Exception(f'{country} Country not found in the list')
+            
+            df = aggregate_quantity_by_hour(df, 'LOAD', 'Load')
+
+            # Formulate the output file path based on the country_id
+            output_file = os.path.join(output_dir, f'LOAD_{country}.csv')
+            
+            # Save the group_data to the file
+            save_data(df, output_file)
+        print('Load files processed, file saved')
 
 if __name__ == "__main__":
     args = parse_arguments()
     # main(args.input_folder, args.output_file)
-    main()
-    # main('./data/ggen/', './data/ggen/process/')
+    # process_gen_data(input_folder='./data/gen_data/', output_dir='./processed_data/')
+    process_load_data(input_folder='./data/load_data/', output_dir='./processed_data/')
